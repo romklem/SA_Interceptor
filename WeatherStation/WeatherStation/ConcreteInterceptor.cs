1bc23160fb3fb744b1d1db5dc3ab29c98e4e112e
romklem <22235884@studentmail.ul.ie>
2023-02-26 10:52:30 +0000

    Implemented concrete interceptor placeholder

---
+++ WeatherStation/WeatherStation/ConcreteInterceptor.cs
@@ -0,0 +1,17 @@
+﻿using WeatherDataLib;
+
+namespace WeatherStation
+{
+    class ConcreteInterceptor : IInterceptor
+    {
+        public void OnPostSensorRead(IContextObject context)
+        {
+            Console.WriteLine("_____Concrete OnPostSensorRead called");
+        }
+
+        public void OnPreSensorDataSent(IContextObject context)
+        {
+            Console.WriteLine("_____Concrete OnPreSensorDataSent called");
+        }
+    }
+}
