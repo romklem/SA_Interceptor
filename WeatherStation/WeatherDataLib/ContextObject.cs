e96d082d675627be5d8855a25ca49135495fefdf
romklem <22235884@studentmail.ul.ie>
2023-02-26 10:25:07 +0000

    Defined ContextObject iface and impl

---
+++ WeatherStation/WeatherDataLib/ContextObject.cs
@@ -0,0 +1,32 @@
+﻿namespace WeatherDataLib
+{
+    internal class ContextObject : IContextObject
+    {
+        // Framework handle
+        private WeatherData _data;
+
+        public ContextObject(WeatherData data)
+        {
+            _data = data;
+        }
+
+        // breaks encapsulation entirely, but it's just a demo..
+        public double Temperature 
+        {
+            get { return _data._temp; }
+            set { _data._temp = value; }
+        }
+
+        public double Pressure
+        {
+            get { return _data._pressure; }
+            set { _data._pressure = value; }
+        }
+
+        public double Humidity
+        {
+            get { return _data._humidity; }
+            set { _data._humidity = value; }
+        }
+    }
+}
---
+++ WeatherStation/WeatherDataLib/IContextObject.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherDataLib
+{
+    public interface IContextObject
+    {
+        double Temperature { get; set; }
+        double Humidity { get; set; }
+        double Pressure { get; set; }
+    }
+}
