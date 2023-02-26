c3614b955a6f37bba7e0aca87ef9797fb34e2935
romklem <22235884@studentmail.ul.ie>
2023-02-26 10:56:32 +0000

    Wired up interceptors with framework

--- WeatherStation/WeatherStation/Program.cs
+++ WeatherStation/WeatherStation/Program.cs
@@ -8,10 +8,22 @@ namespace WeatherStation
     {
         static void Main(string[] args)
         {
+            // concrete framework
             var weatherData = new WeatherData();
-            var display = new CurrentConditionsDisplay();
 
-            weatherData.RegisterObserver(display);
+            // client using data from framework
+            var displayClient = new CurrentConditionsDisplay();
+
+            // concrete interceptor providing out-of-band encryption and conversion services
+            var interceptor = new ConcreteInterceptor();
+
+            // attach interceptor to dispatcher
+            weatherData.Dispatcher.RegisterInterceptor(interceptor);
+
+            // register client with framework (original Observer pattern)
+            weatherData.RegisterObserver(displayClient);
+
+            // will provide 10 measurements for demo
             weatherData.Start();
         }
     }
