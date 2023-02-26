8984d902b8c6a06d23e9bfb8d2e5462bb42f1af7
romklem <22235884@studentmail.ul.ie>
2023-02-26 10:40:18 +0000

    Added Interceptor iface and dispatcher impl

---
+++ WeatherStation/WeatherDataLib/Dispatcher.cs
@@ -0,0 +1,33 @@
+﻿namespace WeatherDataLib
+{
+    public class Dispatcher : IInterceptor  // use same interface for callbacks
+    {
+        private List<IInterceptor> _interceptors = new List<IInterceptor>();
+
+        public void OnPostSensorRead(IContextObject context)
+        {
+            foreach(var interceptor in _interceptors)
+            {
+                interceptor.OnPostSensorRead(context);
+            }
+        }
+
+        public void OnPreSensorDataSent(IContextObject context)
+        {
+            foreach(var interceptor in _interceptors)
+            {
+                interceptor.OnPreSensorDataSent(context);
+            }
+        }
+
+        public void RegisterInterceptor(IInterceptor interceptor)
+        {
+            _interceptors.Add(interceptor);
+        }
+
+        public void UnregisterInterceptor(IInterceptor interceptor)
+        {
+            _interceptors.Remove(interceptor);
+        }
+    }
+}
---
+++ WeatherStation/WeatherDataLib/IInterceptor.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherDataLib
+{
+    // normally would call it ReadAndSendSensorDataInterceptor
+    public interface IInterceptor
+    {
+        void OnPostSensorRead(IContextObject context);
+        void OnPreSensorDataSent(IContextObject context);
+    }
+}
